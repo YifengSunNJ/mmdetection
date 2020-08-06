6a021306e13a3f7c4a40ffdaab41e2a699ebdebe
PadX <40377608+Paddy-Xu@users.noreply.github.com>
2020-08-06 16:38:48 +0200

    Update htc_without_semantic_r50_fpn_1x_coco.py

--- configs/htc/htc_without_semantic_r50_fpn_1x_coco.py
+++ configs/htc/htc_without_semantic_r50_fpn_1x_coco.py
@@ -111,7 +111,7 @@ model = dict(
                 num_convs=4,
                 in_channels=256,
                 conv_out_channels=256,
-                num_classes=80,
+                num_classes=1,
                 loss_mask=dict(
                     type='CrossEntropyLoss', use_mask=True, loss_weight=1.0)),
             dict(
@@ -119,7 +119,7 @@ model = dict(
                 num_convs=4,
                 in_channels=256,
                 conv_out_channels=256,
-                num_classes=80,
+                num_classes=1,
                 loss_mask=dict(
                     type='CrossEntropyLoss', use_mask=True, loss_weight=1.0)),
             dict(
@@ -127,7 +127,7 @@ model = dict(
                 num_convs=4,
                 in_channels=256,
                 conv_out_channels=256,
-                num_classes=80,
+                num_classes=1,
                 loss_mask=dict(
                     type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))
         ]))
